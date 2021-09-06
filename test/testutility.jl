c1bb26024978dd43e00f6c401664bbe89ee0cc01
jbytecode <mhsatman@gmail.com>
2021-09-06 15:35:39 +0300

    add test for euclidean

--- test/testutility.jl
+++ test/testutility.jl
@@ -5,4 +5,9 @@ import GreyDecision.GreyNumbers: GreyNumber
     @testset "Distance to origin" begin
         @test Utility.euclidean(GreyNumber()) == 0.0        
     end
+    @testset "Distance to two grey numbers" begin
+        @test Utility.euclidean(
+            GreyNumber(0.0, 0.0),
+            GreyNumber(0.0, 1.0)) == 1.0        
+    end
 end
