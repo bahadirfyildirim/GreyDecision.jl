b30784fe95f6b6561b775375f187728b54665b27
jbytecode <mhsatman@gmail.com>
2021-09-06 14:45:54 +0300

    new GreyNumber operators

--- src/greynumber.jl
+++ src/greynumber.jl
@@ -1,3 +1,8 @@
+"""
+Liu S., Yang Y., Forrest J. (2017) Grey Numbers and Their Operations. In: Grey Data Analysis. 
+Computational Risk Management. Springer, Singapore. https://doi.org/10.1007/978-981-10-1841-1_3
+"""
+
 struct GreyNumber{T<:Real}
     a::T
     b::T
@@ -30,12 +35,14 @@ function Base.:*(g1::GreyNumber, g2::GreyNumber)::GreyNumber
     )
 end
 
-function Base.:*(g1::GreyNumber, k::T where {T <: Real})::GreyNumber
-    return GreyNumber(g1.a * k, g1.b * k)
+function Base.:*(k::T where {T <: Number}, g1::GreyNumber)::GreyNumber
+    a = g1.a * k
+    b = g1.b * k
+    return GreyNumber(min(a, b), max(a,b))
 end
 
-function Base.:*(k::T where {T <: Real}, g1::GreyNumber)::GreyNumber
-    return GreyNumber(g1.a * k, g1.b * k)
+function Base.:*(g1::GreyNumber, k::T where {T <: Number})::GreyNumber
+    return k * g1
 end
 
 function Base.inv(g::GreyNumber)::GreyNumber
@@ -56,8 +63,62 @@ function Base.:/(g1::GreyNumber, g2::GreyNumber)::GreyNumber
     )
 end
 
+function Base.:/(k::T where {T <: Real}, g1::GreyNumber)::GreyNumber
+    return g1 / GreyNumber(k, k)
+end
+
+function Base.:/(g1::GreyNumber, k::T where {T <: Real})::GreyNumber
+    return g1 / GreyNumber(k, k)
+end
+
 function Base.:^(g::GreyNumber, k::T where {T <: Real})::GreyNumber
     @assert k > 0
     @assert g.a <= g.b
-    return GreyNumber(g.a^k, g.b^k)
+    anew = g.a^k
+    bnew = g.b^k
+    return GreyNumber(min(anew, bnew), max(anew, bnew))
+end
+
+function Base.isless(g1::GreyNumber, g2::GreyNumber)::Bool
+    @assert g1.a <= g1.b 
+    @assert g2.a <= g2.b
+    return g1.a < g2.a 
+end
+
+function Base.:≤(g1::GreyNumber, g2::GreyNumber)::Bool
+    @assert g1.a <= g1.b 
+    @assert g2.a <= g2.b
+    return g1.a <= g2.a 
+end
+
+function Base.isgreater(g1::GreyNumber, g2::GreyNumber)::Bool
+    @assert g1.a <= g1.b 
+    @assert g2.a <= g2.b
+    return g1.b > g2.b 
+end
+
+function Base.:≥(g1::GreyNumber, g2::GreyNumber)::Bool
+    @assert g1.a <= g1.b 
+    @assert g2.a <= g2.b
+    return g1.b >= g2.b 
+end
+
+function Base.isequal(g1::GreyNumber, g2::GreyNumber)::Bool
+    return (g1.a == g2.a) && (g1.b == g2.b) 
+end
+
+function Base.isreal(g::GreyNumber)::Bool
+    return isreal(g.a)
+end
+
+function Base.isinteger(g::GreyNumber)::Bool
+    return isinteger(g.a)
+end
+
+function Base.isinf(g::GreyNumber)::Bool 
+    return isinf(g.a) || isinf(g.b)
+end
+
+function Base.iszero(g::GreyNumber)::Bool
+    return iszero(g.a) && iszero(g.b)
 end
