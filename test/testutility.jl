8043bcea8ea6de18e252518c8b1ca62fbed7bf74
jbytecode <mhsatman@gmail.com>
2021-09-07 14:33:03 +0300

    add new tests for apply

--- test/testutility.jl
+++ test/testutility.jl
@@ -24,6 +24,30 @@ end
         @test result[1] == GreyNumber(8, 11)
         @test result[2] == GreyNumber(80, 110)
     end
+    
+    @testset "Apply minimum to columns" begin
+        greys = [
+            GreyNumber(1, 2) GreyNumber(10, 20);
+            GreyNumber(3, 4) GreyNumber(30, 40);
+            GreyNumber(4, 5) GreyNumber(40, 50);
+        ]
+        result = Utility.apply_columns(minimum, greys)
+        @test length(result) == 2
+        @test result[1] == GreyNumber(1, 2)
+        @test result[2] == GreyNumber(10, 20)
+    end
+    
+     @testset "Apply maximum to columns" begin
+        greys = [
+            GreyNumber(1, 2) GreyNumber(10, 20);
+            GreyNumber(3, 4) GreyNumber(30, 40);
+            GreyNumber(4, 5) GreyNumber(40, 50);
+        ]
+        result = Utility.apply_columns(maximum, greys)
+        @test length(result) == 2
+        @test result[1] == GreyNumber(4, 5)
+        @test result[2] == GreyNumber(40, 50)
+    end
 end
 
 
@@ -39,5 +63,31 @@ end
         @test result[1] == GreyNumber(11, 22)
         @test result[2] == GreyNumber(33, 44)
         @test result[3] == GreyNumber(44, 55)
+    end
+
+     @testset "Apply minimum to rows" begin
+        greys = [
+            GreyNumber(1, 2) GreyNumber(10, 20);
+            GreyNumber(3, 4) GreyNumber(30, 40);
+            GreyNumber(4, 5) GreyNumber(40, 50);
+        ]
+        result = Utility.apply_rows(minimum, greys)
+        @test length(result) == 3
+        @test result[1] == GreyNumber(1, 2)
+        @test result[2] == GreyNumber(3, 4)
+        @test result[3] == GreyNumber(4, 5) 
+    end
+    
+     @testset "Apply maximum to rows" begin
+        greys = [
+            GreyNumber(1, 2) GreyNumber(10, 20);
+            GreyNumber(3, 4) GreyNumber(30, 40);
+            GreyNumber(4, 5) GreyNumber(40, 50);
+        ]
+        result = Utility.apply_rows(maximum, greys)
+        @test length(result) == 3
+        @test result[1] == GreyNumber(10, 20)
+        @test result[2] == GreyNumber(30, 40)
+        @test result[3] == GreyNumber(40, 50) 
     end
 end
