5bb355abf92b5b706d1e3848e00290052c76c0a2
jbytecode <mhsatman@gmail.com>
2021-09-09 16:34:52 +0300

    (#3) Topsis initials, importing modules

--- src/topsis.jl
+++ src/topsis.jl
@@ -1,11 +1,14 @@
 module Topsis
 
 import GreyDecision.GreyNumbers.GreyNumber
+import GreyDecision.Utility
 
 # Test - remove this
 g = GreyNumber()
 @info g
 
+# Test -remove this
+# something = Utility.makeminmax(...)
 
 
 end # End of module Topsis
