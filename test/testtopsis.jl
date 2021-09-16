7b7956e3cfecec4cda4c79140479e7aa3ce675c0
jbytecode <mhsatman@gmail.com>
2021-09-17 02:20:30 +0300

    (#3) our older tests pass when upper and lower bounds are equal (reals)

--- test/testtopsis.jl
+++ test/testtopsis.jl
@@ -16,3 +16,21 @@ import GreyDecision.Topsis: topsis
     @test scores[2] == GreyNumber{Float64}(0.24317523558639148, 1.002942207810138)
     @test scores[3] == GreyNumber{Float64}(0.10851899761349458, 1.23913068959885)
 end
+
+@testset "Grey Topsis with real numbers" begin
+    num6 = GreyNumber{Float64}(6.0, 6.0)
+    num7 = GreyNumber{Float64}(7.0, 7.0)
+    num8 = GreyNumber{Float64}(8.0, 8.0)
+    num9 = GreyNumber{Float64}(9.0, 9.0)
+    decmat = [
+        num9 num7 num6 num7;
+        num8 num7 num9 num6;
+        num7 num8 num6 num6
+    ]
+    w = [4.0, 2, 6, 8]
+    fns = makeminmax([maximum, maximum, maximum, maximum])
+    result = topsis(decmat, w, fns)
+    @test result[1] == GreyNumber{Float64}(0.3876869549221182, 0.3876869549221182)
+    @test result[2] == GreyNumber{Float64}(0.6503238218850165, 0.6503238218850165)
+    @test result[3] == GreyNumber{Float64}(0.08347670030339038, 0.08347670030339038)
+end
